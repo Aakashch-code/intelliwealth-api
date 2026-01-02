7d9fb70c6d343322b1b88dfdef2a5035473cc8ed
Aakash <aakashch.code@gmail.com>
2026-01-02 14:42:28 +0530

    updated database config

--- src/main/java/com/example/intelliwealth/IntelliWealthApplication.java
+++ src/main/java/com/example/intelliwealth/IntelliWealthApplication.java
@@ -2,12 +2,30 @@ package com.example.intelliwealth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+
+// PostgreSQL (Core)
+@EntityScan(basePackages = "com.example.intelliwealth.core")
+@EnableJpaRepositories(
+        basePackages = "com.example.intelliwealth.core"
+)
+
+// MongoDB (Wealth + Protection + Fynix)
+@EnableMongoRepositories(
+        basePackages = {
+                "com.example.intelliwealth.wealth",
+                "com.example.intelliwealth.protection",
+                "com.example.intelliwealth.fynix"
+        }
+)
+
 public class IntelliWealthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IntelliWealthApplication.class, args);
     }
-
 }
