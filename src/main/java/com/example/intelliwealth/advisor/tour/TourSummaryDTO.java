374db9374ee1e1b0623d497549d3caa072eb88cf
Aakash <aakashch.code@gmail.com>
2026-02-11 15:11:10 +0530

    hold: pause experimental tour service development

--- src/main/java/com/example/intelliwealth/advisor/tour/TourSummaryDTO.java
+++ src/main/java/com/example/intelliwealth/advisor/tour/TourSummaryDTO.java
@@ -1,20 +1,13 @@
 package com.example.intelliwealth.advisor.tour;
 
-import com.example.intelliwealth.protection.contingency.ContingencyReportDTO;
-import com.example.intelliwealth.treasury.budget.application.dto.BudgetSummaryDTO;
-import com.example.intelliwealth.treasury.goal.application.dto.GoalStatsResponseDTO;
-import com.example.intelliwealth.treasury.transaction.application.dto.SavingResponse;
-import com.example.intelliwealth.wealth.debt.application.dto.DebtStatsDTO;
 import lombok.Data;
-
-import java.util.List;
 
 @Data
 public class TourSummaryDTO {
-    private String timeframe = "The collected data is from all time.";
-    private BudgetSummaryDTO budgetSummary;
-    private ContingencyReportDTO contingency;
-    private SavingResponse netPosition;
-    private DebtStatsDTO debtSummary;
-    private GoalStatsResponseDTO goalStats;
+//    private String timeframe = "The collected data is from all time.";
+//    private BudgetSummaryDTO budgetSummary;
+//    private ContingencyReportDTO contingency;
+//    private SavingResponse netPosition;
+//    private DebtStatsDTO debtSummary;
+//    private GoalStatsResponseDTO goalStats;
 }
